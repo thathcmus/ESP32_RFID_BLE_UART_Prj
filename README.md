02b8ae8949a26ccc0dd62059bdcbd5c371613439
thathcmus <thathcmus@gmail.com>
2022-12-12 14:04:21 +0700

    version 2.0

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
-# ESP32_RFID_BLE_UART_Prj
-    
-Công việc của mỗi người như sau:
-
-
-	
-Thực hiên đọc thẻ từ RFID để mở cửa: HUỲNH CHÍ THẬT, TRẦN NGUYỄN HOÀ THUẬN
-	
+# ESP32_RFID_BLE_UART_Prj	
 Thực hiện phát BLE trên ESP32 kết nối đến phone để nhập mật khẩu mở cửa: Tùng, NGUYỄN CHÍ THÀNH
-	
-Thực hiện UART hiển thị thông tin trên màn hình máy tính và các tính năng trên đó: PHAN TÂN
+- Card list:
++ White Card:   161 64 42 29
++ Blue Card 1:  51 190 251 182
++ Blue Card 2:  58 40 69 41
--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -11,49 +11,42 @@ MISO (Master Input Slave Output)    <-> D19
 MOSI (Master Output Slave Input)    <-> D23
 SCK (Serial Clock)                  <-> D18
 SS/SDA (Slave select)               <-> D5
-LED (DOOR)                          <-> D2
+LED (DOOR)                          <-> D27
 */
-#define SS_PIN  5
+#define SS_PIN 5
 #define RST_PIN 4
-#define LED_PIN 2
+#define LED_PIN 27
+
+#define WHITE_CARD
+#define BLUE_CARD_1
+// #define BLUE_CARD_2
 
 //Variables
 byte nuidPICC[4] = { 0, 0, 0, 0 };
-byte nuid_saved_1[4] = { 1, 2, 3, 4 };
+#ifdef WHITE_CARD
+byte white_card[4] = { 161, 64, 42, 29 };
+#endif
+
+#ifdef BLUE_CARD_1
+byte blue_card_1[4] = { 51, 190, 251, 182};
+#endif
+
+#ifdef BLUE_CARD_2
+byte blue_card_2[4] = { 58, 40, 69, 41 };
+#endif
+
 MFRC522::MIFARE_Key key;
 MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
 
 //Init timer
-hw_timer_t* timer = NULL;
+hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-
-
-// Interrupt Handler
-void IRAM_ATTR onTimer() {   
-  portENTER_CRITICAL_ISR(&timerMux);
-  digitalWrite(LED_PIN, LOW);
-  if(timer)
-  {
-    timerEnd(timer);
-    timer = NULL;
-  }
-  portEXIT_CRITICAL_ISR(&timerMux);
-}
 
 void setup() {
   //Init Pin
   pinMode(LED_PIN, OUTPUT);
-  
-  //1us each time
-  timer = timerBegin(0, 80, true);
-  //Init Handler Timer
-  timerAttachInterrupt(timer, &onTimer, true);
-  //Init interrupt timer 5s
-  timerWrite(timer, 5000000);
 
-  //Timer: https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/api/timer.html
-
-  //Init Serial USB
+  //Init Serial USBS
   Serial.begin(115200);
   Serial.println(F("Initialize System"));
 
@@ -72,72 +65,49 @@ void loop() {
   runRFID();
 }
 
-// Function to Check NUID of card
-void readRFID(void) {
-  ////Read RFID card
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-  // Look for new 1 cards
+//Read NUID and Run features.
+void runRFID() {
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if (!rfid.PICC_IsNewCardPresent())
     return;
+
   // Verify if the NUID has been readed
   if (!rfid.PICC_ReadCardSerial())
     return;
-  // Store NUID into nuidPICC array
-  for (byte i = 0; i < 4; i++) {
-    nuidPICC[i] = rfid.uid.uidByte[i];
+
+  if (
+#ifdef WHITE_CARD
+    (rfid.uid.uidByte[0] == white_card[0] 
+    && rfid.uid.uidByte[1] == white_card[1] 
+    && rfid.uid.uidByte[2] == white_card[2] 
+    && rfid.uid.uidByte[3] == white_card[3])
+#endif
+    
+#ifdef BLUE_CARD_1
+    ||
+    (rfid.uid.uidByte[0] == blue_card_1[0] 
+    && rfid.uid.uidByte[1] == blue_card_1[1] 
+    && rfid.uid.uidByte[2] == blue_card_1[2] 
+    && rfid.uid.uidByte[3] == blue_card_1[3])
+#endif
+    
+#ifdef BLUE_CARD_2
+    ||
+    (rfid.uid.uidByte[0] == blue_card_2[0] 
+    && rfid.uid.uidByte[1] == blue_card_2[1] 
+    && rfid.uid.uidByte[2] == blue_card_2[2] 
+    && rfid.uid.uidByte[3] == blue_card_2[3])
+#endif
+    ) 
+  {
+    Serial.println(F("Correct card -> The door opened!"));
+    digitalWrite(LED_PIN, HIGH);
+    delay(5000);
+    Serial.println(F("The door closed!"));
   }
-  Serial.print(F("RFMFRC522ID In dec: "));
-  printDec(rfid.uid.uidByte, rfid.uid.size);
-  Serial.println();
-  // Halt PICC
-  rfid.PICC_HaltA();
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
-}
 
-//Read NUID and Run features.
-void runRFID()
-{
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
-
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-
-  if (rfid.uid.uidByte[0] == nuid_saved_1[0] &&
-    rfid.uid.uidByte[1] == nuid_saved_1[1] &&
-    rfid.uid.uidByte[2] == nuid_saved_1[2] &&
-    rfid.uid.uidByte[3] == nuid_saved_1[3] ) {
-      Serial.println(F("Correct card. The door opened!"));
-      digitalWrite(LED_PIN, HIGH);
-      timerStart(timer);
-    }
-
-  else{
-    Serial.println(F("Incorect card. The door still close!!"));
-    }
-}
-/**
- 		Helper routine to dump a byte array as hex values to Serial.
-*/
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+  else {
+    Serial.println(F("Incorrect card -> The door is still closed!!!"));
   }
+  digitalWrite(LED_PIN, LOW);
 }
-/**
- 		Helper routine to dump a byte array as dec values to Serial.
-*/
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], DEC);
-  }
-}
-
-//https://deviot.vn/tutorials/esp32.66047996/ngat-timer.67695749
