73ce891e3340aa87ab6c76ddad98142c9fe3b33f
TanPhan <phantanbn31@gmail.com>
2022-12-12 09:38:51 +0700

    update uart

--- doan.ino
+++ doan.ino
@@ -1,33 +1,54 @@
 #define LEDPIN 2
 String cmd;
-String passwordTemp = "abc";
+char passwordTemp[10] ="" ;
+char password[10] = "abcdef";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(LEDPIN,OUTPUT);
-  Serial.println("Hello, ESP32!");
 }
+
 void loop() {
   Serial.println("=================================================================");
   Serial.println("\t\t1.Type password to open your door");
   Serial.println("\t\t2.Change your password");
   Serial.println("\t\t3.Close your door");
   Serial.print("\t\tEnter your choose:");
-    while (Serial.available() == 0) {}     //wait for data available
-  cmd = Serial.readString();  //read until timeout
+  while (Serial.available() == 0) {}     //wait for data available
+  cmd = Serial.readString();
   cmd.trim(); 
-  Serial.println(cmd);                       // remove any \r \n whitespace at the end of the String
+  Serial.println(cmd);                     g
+
+
   if(cmd == "1")
   {
-    Serial.println("=================================================================");
-    Serial.print("Type Your Password:");
-    for(int i = 1; i<= 3; i++)
+    getPassToOPenDoor();
+  }
+  else if(cmd == "2")
+  {
+    changePass();
+  }else if(cmd == "3")
+  {
+    closeDoor();
+  }
+ 
+}
+
+void convertCharToString(){
+
+}
+
+void getPassToOPenDoor() {
+  Serial.println("=================================================================");
+  Serial.print("Type Your Password:");
+  for(int i = 1; i<= 3; i++)
     {
       while (Serial.available() == 0) {}     //wait for data available
       cmd = Serial.readString(); 
       cmd.trim();  
-      Serial.println(cmd);  
-      if(cmd != passwordTemp){
+      cmd.toCharArray(passwordTemp,cmd.length() + 1);
+      Serial.println(passwordTemp);
+      if(strcmp(password,passwordTemp)){
           Serial.print("\t\t\tWRONG PASSWORD: ");
           Serial.println(i);
           if(i<3)
@@ -43,17 +64,17 @@ void loop() {
           break;
       }
     }
-  }
-  else if(cmd == "2")
-  {
+}
+
+void changePass(){
       Serial.print("Type your old Password: ");
        for(int i = 1;i <= 3; i++){
            while (Serial.available() == 0) {}  
             cmd = Serial.readString(); 
             cmd.trim();
+            cmd.toCharArray(passwordTemp,cmd.length() + 1);
             Serial.println(cmd);
-
-            if(cmd != passwordTemp)
+            if(strcmp(passwordTemp,password))
               {
                   Serial.print("\t\t\tWRONG PASSWORD: ");
                   Serial.println(i);
@@ -71,17 +92,17 @@ void loop() {
                   while (Serial.available() == 0) {}  
                   cmd = Serial.readString(); 
                   cmd.trim();
-                  Serial.println(cmd);
-                  passwordTemp = cmd;
+                  cmd.toCharArray(passwordTemp,cmd.length() + 1);
+                  Serial.println(passwordTemp);
+                  strcpy(password,passwordTemp);
                   Serial.println("\t\tCHANGE YOUR NEW PASSWORD SUCCESS");
                   break;
             }
-  }
-  }else if(cmd == "3")
-  {
+      }
+}
+
+void closeDoor(){
     Serial.println("=================================================================");
     digitalWrite(LEDPIN, LOW);
     Serial.println("\t\t\tYOUR DOOR IS CLOSED");
-  }
-
 }
